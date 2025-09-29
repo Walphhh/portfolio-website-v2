fa07a24969353193e6a725d45dc5f8bb2fcc523f
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-09-29 23:30:05 +1000

    export changes

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -45,44 +45,6 @@ const Navbar = () => {
         scrollTo={scrollTo}
       />
     </>
-
-    // {/* Mobile Navbar (hamburger at bottom) */}
-    // <div className="md:hidden fixed bottom-5 left-1/2 -translate-x-1/2 z-50 z-[9999]">
-    //   {/* Hamburger button */}
-    //   <div className="flex items-center space-x-4 bg-[var(--layer-1)] rounded-2xl px-4 py-2 shadow-md">
-    //     <button onClick={() => setMobileOpen(!mobileOpen)}>
-    //       {mobileOpen ? (
-    //         <X className="size-6" />
-    //       ) : (
-    //         <Menu className="size-6" />
-    //       )}
-    //     </button>
-    //     <ThemeToggle />
-    //   </div>
-
-    //   {/* Dropdown menu */}
-    //   {mobileOpen && (
-    //     <div className="absolute bottom-16 left-1/2 -translate-x-1/2 w-56 bg-[var(--layer-1)] rounded-2xl shadow-lg flex flex-col p-4 space-y-2">
-    //       {NavList.map((item, idx) => (
-    //         <button
-    //           key={item.name}
-    //           onClick={() => {
-    //             setCurrentIndex(idx);
-    //             setMobileOpen(false);
-    //             if (item.linkTo) {
-    //               window.location.href = item.linkTo;
-    //             } else if (item.sectionID) {
-    //               scrollTo(item.sectionID);
-    //             }
-    //           }}
-    //           className="py-2 px-3 rounded-lg hover:bg-accent text-center"
-    //         >
-    //           {item.name}
-    //         </button>
-    //       ))}
-    //     </div>
-    //   )}
-    // </div>
   );
 };
 
